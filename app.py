1e7cdac95bd046c3dfaf1b9ae4a08e7cb68106c0
Rahul Singh Dani <76247324+RahulSinghDani@users.noreply.github.com>
2024-06-28 11:08:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import streamlit as st
+import os
 
-# Read the HTML content
-with open("./index.html", "r", encoding="utf-8") as f:
-    html_content = f.read()
+def main():
+    # Read the HTML content
+    with open(os.path.join('app', 'index.html'), "r", encoding="utf-8") as f:
+        html_content = f.read()
 
-# Display the HTML content using Streamlit's `st.components.v1.html` function
-st.components.v1.html(html_content)
+    # Display the HTML content using Streamlit's `st.components.v1.html` function
+    st.components.v1.html(html_content)
+
+if __name__ == "__main__":
+    main()
