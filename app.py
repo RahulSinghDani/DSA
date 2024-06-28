af536f6948a9f44fb0d4975087c8b58fa82533d7
Rahul Singh Dani <76247324+RahulSinghDani@users.noreply.github.com>
2024-06-28 11:28:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -202,7 +202,7 @@ def main():
 """
 
     # Display the HTML content using Streamlit's `st.components.v1.html` function
-    st.components.v1.html(html_content, height=800)
+    st.components.v1.html(html_content, height="100%")
 
 if __name__ == "__main__":
     main()
